43ba87e750cefa46c2df2f0984f8d386d0968e21
1vxnValencia <ivanaguilar152002@gmail.com>
2023-11-06 21:53:29 -0600

    004

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -6,26 +6,42 @@ const Counter = () => {
         counter2: 2,
         counter3: 0
     })
-
-    const { counter1, counter2, counter3 } = state
-    const contador = (contador_num)=>{
-        if (contador_num==1) setCounter({ counter1:counter1+1, counter2:counter2, counter3:counter3});
-        if (contador_num==2) setCounter({ counter1:counter1, counter2:counter2+1, counter3:counter3});
-        if (contador_num==2) setCounter({ counter1:counter1, counter2:counter2, counter3:counter3+1});
+    
+    const contador1 = ()=>{
+        setCounter({
+            ...state,
+            counter1: counter1+1
+        });
     }
+    const contador2 = ()=>{
+        setCounter({
+            ...state,
+            counter2: counter2+1
+        });
+    }
+    const contador3 = ()=>{
+        setCounter({
+            ...state,
+            counter3: counter3+1
+        });
+    }
+    const { counter1, counter2, counter3 } = state
     return (
         <div>
             <h3>Counter App</h3>
             <h5>Counter A: {counter1}</h5>
             <h5>Counter B: {counter2}</h5>
             <h5>Counter C: {counter3}</h5>
-            <button onClick={contador(1)} className='btn btn-primary'>
+            <button
+                className='btn btn-primary'
+                onClick={contador1}
+            >
                 A+1
             </button>
-            <button onClick={contador(2)} className='btn btn-primary'>
+            <button onClick={contador2} className='btn btn-primary'>
                 B+1
             </button>
-            <button onClick={contador(3)} className='btn btn-primary'>
+            <button onClick={contador3} className='btn btn-primary'>
                 C+1
             </button>
             
